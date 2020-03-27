0e14d6b9655c80305150dd935ec438d023a6dd74
Son Mai <son.mai@codegym.vn>
2020-03-27 16:07:23 +0700

    update

--- CrawlSongExample.cs
+++ CrawlSongExample.cs
@@ -1,6 +1,5 @@
 ﻿using HtmlAgilityPack;
-using System;
-using System.Collections.Generic;
+using System; 
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -11,31 +10,21 @@ namespace RegularExpressions
     {
         static void Main()
         {
-            HtmlWeb htmlWeb = new HtmlWeb()
-            {
-                AutoDetectEncoding = false, 
-            };
+            HtmlWeb htmlWeb = new HtmlWeb();
             Console.OutputEncoding = Encoding.UTF8;
             //Load trang web, nạp html vào document
             HtmlDocument document = htmlWeb.Load("https://www.nhaccuatui.com/bai-hat/nhac-tre-moi.html");
             //Load các tag li trong tag ul
             var listGenre = document.DocumentNode.SelectNodes("//ul[@class='listGenre']/li").ToList();
-
-            var items = new List<object>();
-            string getAtag = @"<\s*a[^>]*>(.*?)";
-            string getTitleAttribute = @"title+\W\D+";
-            string songNamePatern = @"'+\w+\D+\s";
+ 
             foreach (var item in listGenre)
             {
-                Match match = Regex.Match(item.InnerHtml, getAtag);
-                string titleAttribute = Regex.Match(match.Value, getTitleAttribute).Value;
+                Match match = Regex.Match(item.InnerHtml, @"<\s*a[^>]*>(.*?)");
+                string titleAttribute = Regex.Match(match.Value, @"title+\W\D+").Value;
                 titleAttribute = titleAttribute.Replace("\"", "'");
-                string songName = Regex.Match(titleAttribute, songNamePatern).Value.Replace("'", "");
+                string songName = Regex.Match(titleAttribute, @"'+\w+\D+\s").Value.Replace("'", "");
                 Console.WriteLine(songName);
-
             }
-
         }
-
     }
 }
